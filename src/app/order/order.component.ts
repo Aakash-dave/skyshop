05512e8b5ad252c43b6553a7d3be2f1485a5b547
Aakash Dave <aakash.dave81@gmail.com>
2022-12-22 21:58:26 +0530

    Order page amount section

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -7,6 +7,7 @@ export interface IOrderDetail {
   price: string,
   category: string,
   url1: string,
+  quantity?: number
 }
 
 @Component({
@@ -15,10 +16,16 @@ export interface IOrderDetail {
   styleUrls: ['./order.component.css'],
 })
 
-export class OrderComponent implements OnInit {  
+export class OrderComponent implements OnInit {
 
   cartIds!: number[];
   cartProducts: IOrderDetail[] = [];
+  cartQuantity = new Map();
+
+  cartPrice: number = 0;
+  deliveryCharge: number = 0;
+  discount: number = 5;
+  cartTotal: number = 0;
 
   constructor(private _dataService: DataService) {
     this._dataService.cartItemId_sub.subscribe({
@@ -30,14 +37,44 @@ export class OrderComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.cartIds = [1,2,3,4];
+    this.cartIds = [1, 2, 3, 4];
 
     this._dataService.getcartItems(this.cartIds).subscribe({
-      next: (req:any) => {
+      next: (req: any) => {
         this.cartProducts = req;
-        console.log(this.cartProducts);
+        // console.log(this.cartProducts);
       },
+      complete: () => {
+        this.cartPrice = this.cartProducts.reduce((accumulator: number, ele: any) =>
+          (+accumulator + parseInt(ele.price)) * (1), 0);
+          this.recalTotal();
+      }
     });
 
+    //
+    this.cartIds.forEach(ele => {
+      this.cartQuantity.set(ele, 1);
+    })
   }
+
+  increment(item_id: number, price: string) {
+    if (this.cartQuantity.get(item_id) < 10) {
+      this.cartQuantity.set(item_id, this.cartQuantity.get(item_id) + 1);
+      this.cartPrice += parseInt(price);
+    }
+    this.recalTotal();
+  }
+
+  decrement(item_id: number, price: string) {
+    if (this.cartQuantity.get(item_id) > 1) {
+      this.cartQuantity.set(item_id, this.cartQuantity.get(item_id) - 1);
+      this.cartPrice -= parseInt(price);
+    }
+    this.recalTotal();
+  }
+
+  recalTotal() {
+    this.cartTotal = this.cartPrice - ((this.cartPrice * this.discount) / 100) - this.deliveryCharge;
+  }
+
 }
